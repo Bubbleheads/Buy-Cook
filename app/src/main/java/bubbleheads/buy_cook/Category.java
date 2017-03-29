7fa6225e3ddd30ae111c9b4b60448b02034c7e53
Marta Liutyk <melodika99@gmail.com>
2017-03-29 17:32:08 +0300

    Added an enum with categories

---
+++ app/src/main/java/bubbleheads/buy_cook/Category.java
@@ -0,0 +1,16 @@
+package bubbleheads.buy_cook;
+
+public enum Category {
+
+    SOUPS("Soups"), PIZZA ("pizza"), MEAT ("meat"),SALADS("Salads"), LUNCH("Lunch"), MAIN_DISHES ("Main dishes"), DRINKS ("Drinks"), DESSERTS("Desserts"), COOKIES("Cookies"),CAKES("Cakes"),PIES ("Pies"),VEGAN("Vegan"),VEGETERIAN("Vegeterian");
+
+    private final String name;
+
+    Category (final String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}

45b75e231545f69c4046e1426f24e3ea7a81f093
Marta Liutyk <melodika99@gmail.com>
2017-04-03 23:36:37 +0300

    corrected class Recipe

--- app/src/main/java/bubbleheads/buy_cook/Recipe.java
+++ app/src/main/java/bubbleheads/buy_cook/Recipe.java
@@ -10,39 +10,34 @@ public class Recipe {
     private boolean favorite;
     private final Category category;
 
-    public Recipe(String name, String detail, int photo, Category category){
+    public Recipe(final String name,final String detail,final int photo,final Category category) {
         this.recipeName = name;
         this.recipeDetail = detail;
         this.recipePhoto = photo;
         this.category = category;
     }
 
-    public String getRecipeName(){
+    public String getRecipeName() {
         return this.recipeName;
     }
 
-    public String getRecipeDetail(){
+    public String getRecipeDetail() {
         return this.recipeDetail;
     }
 
-    public int getRecipePhoto(){
+    public int getRecipePhoto() {
         return this.recipePhoto;
     }
 
-    public boolean getFavorite(){
+    public boolean getFavorite() {
         return this.favorite;
     }
 
-    public Category getCategory(){
+    public Category getCategory() {
         return this.category;
     }
 
-    public void changeFavorite(){
-        if (this.favorite == false) {
-            this.favorite = true;
-        }
-        else{
-            this.favorite = false;
-        }
+    public void changeFavorite() {
+      this.favorite =!this.favorite;
     }
 }

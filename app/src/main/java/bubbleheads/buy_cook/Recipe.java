29452b61d73aa6d0c22e17346f0b15c1faa1eb84
Marta Liutyk <melodika99@gmail.com>
2017-04-14 15:15:54 +0300

    corrected design of gridview

--- app/src/main/java/bubbleheads/buy_cook/Recipe.java
+++ app/src/main/java/bubbleheads/buy_cook/Recipe.java
@@ -6,24 +6,15 @@ public class Recipe {
     private final int recipePhoto;
     private final String recipeHowToCook;
     private final Category category;
-<<<<<<< HEAD
-    private final String recipeHowToCook;
-    private boolean favorite;
-
-    public Recipe(final String name, final String detail,
-                  final int photo, final Category category,final String recipeHowToCook) {
-=======
     private boolean favorite;
 
     public Recipe(final String name, final String detail,
                   final int photo, final Category category, final String recipeHowToCook) {
->>>>>>> 1f69cb79b561d4c072fde2fef0c76682d3fbff2a
         this.recipeName = name;
         this.recipeDetail = detail;
         this.recipeHowToCook = recipeHowToCook;
         this.recipePhoto = photo;
         this.category = category;
-        this.recipeHowToCook = recipeHowToCook;
     }
 
     public String getRecipeName() {

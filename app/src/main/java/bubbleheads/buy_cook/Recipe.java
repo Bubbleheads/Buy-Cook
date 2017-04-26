1a3abb2d9c917c2871d00a63d2ad057f0def9c2f
Marvin99 <maksym.yablonskyi@gmail.com>
2017-04-26 17:54:31 +0300

    Update Recipe.java

--- app/src/main/java/bubbleheads/buy_cook/Recipe.java
+++ app/src/main/java/bubbleheads/buy_cook/Recipe.java
@@ -1,5 +1,9 @@
 package bubbleheads.buy_cook;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Recipe {
     private final String recipeName;
     private final String recipeDetail;
@@ -7,41 +11,36 @@ public class Recipe {
     private final String recipeHowToCook;
     private final Category category;
     private boolean favorite;
+    private final Map ingredientMap;
 
     public Recipe(final String name, final String detail,
-                  final int photo, final Category category, final String recipeHowToCook) {
+                  final int photo, final Category category, final String recipeHowToCook, final Map ingredientMap) {
         this.recipeName = name;
         this.recipeDetail = detail;
         this.recipeHowToCook = recipeHowToCook;
         this.recipePhoto = photo;
         this.category = category;
+        this.ingredientMap = ingredientMap;
     }
 
-    public String getRecipeName() {
-        return recipeName;
-    }
+    public String getRecipeName() {return recipeName;}
 
-    public String getRecipeDetail() {
-        return recipeDetail;
-    }
+    public String getRecipeDetail() {return recipeDetail;}
 
-    public int getRecipePhoto() {
-        return recipePhoto;
-    }
+    public int getRecipePhoto() {return recipePhoto;}
 
-    public String getRecipeHowToCook() {
-        return recipeHowToCook;
-    }
+    public String getRecipeHowToCook() {return recipeHowToCook;}
 
-    public boolean getFavorite() {
-        return favorite;
-    }
+    public boolean getFavorite() {return favorite;}
 
-    public Category getCategory() {
-        return category;
-    }
+    public Map getIngredientMap() {return ingredientMap;}
 
-    public void toggleFavorite() {
-        this.favorite = !this.favorite;
-    }
+    public Category getCategory() {return category;}
+
+    public void toggleFavorite() {this.favorite = !this.favorite;}
+
+    public boolean isFavorite() {return favorite;}
 }
+
+
+
